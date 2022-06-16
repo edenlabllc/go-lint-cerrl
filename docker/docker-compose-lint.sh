df649ad61f6d5388a0a836169a93af724f581f1a
Denys Vorona <trap.v.ck@gmail.com>
2022-06-16 17:56:22 +0300

    fix: golangci-lint timeout

--- docker/docker-compose-lint.sh
+++ docker/docker-compose-lint.sh
@@ -8,6 +8,6 @@ go build --buildmode plugin -o /tmp/cerrl.so plugin/cerrl.go
 
 cat config/.golangci.yml | sed "s/path: ..\/cerrl.so/path: \/tmp\/cerrl.so/g" > /tmp/.golangci.yml
 
-golangci-lint run -v --config /tmp/.golangci.yml
+golangci-lint run -v --timeout 10m --config /tmp/.golangci.yml
 
 rm /tmp/cerrl.so
