6fe1f6ae9572616b9c8943fd3e99c1f0338f32f9
s.novytskyi <edii87shadow@gmail.com>
2024-12-18 13:14:17 +0200

    update linter 1.62.2 (#10)
    * update linter 1.62.2

--- docker/docker-compose-lint.sh
+++ docker/docker-compose-lint.sh
@@ -2,11 +2,14 @@
 
 cd /app
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.59.1
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
 
 go build --buildmode plugin -o /tmp/cerrl.so plugin/cerrl.go
 
 cat config/.golangci.yml | sed "s/path: ..\/cerrl.so/path: \/tmp\/cerrl.so/g" > /tmp/.golangci.yml
+
+# To get the version of a specific library
+golangci-lint version --debug | grep "golang.org/x/tools"
 
 golangci-lint run -v --config /tmp/.golangci.yml
 
