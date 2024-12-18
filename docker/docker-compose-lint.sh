840fdc015503804668a611c3d14ce898dee1f7f9
edii <edii87shadow@gmail.com>
2024-12-18 12:31:54 +0200

    update linter 1.62.2

--- docker/docker-compose-lint.sh
+++ docker/docker-compose-lint.sh
@@ -2,11 +2,22 @@
 
 cd /app
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.59.1
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
 
+echo "build..."
 go build --buildmode plugin -o /tmp/cerrl.so plugin/cerrl.go
 
+go version
+
 cat config/.golangci.yml | sed "s/path: ..\/cerrl.so/path: \/tmp\/cerrl.so/g" > /tmp/.golangci.yml
+
+ls -al /tmp
+
+# To get all the information
+golangci-lint version --debug
+
+# To get the version of a specific library
+golangci-lint version --debug | grep "golang.org/x/tools"
 
 golangci-lint run -v --config /tmp/.golangci.yml
 
--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/edenlabllc/go-lint-cerrl
 
-go 1.22
+go 1.23
 
-require golang.org/x/tools v0.22.0
+require golang.org/x/tools v0.27.0
 
 require (
-	golang.org/x/mod v0.18.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
 )
