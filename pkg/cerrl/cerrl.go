2267dcf9532d94a3d6d4317b20381a5955160185
s.novytskyi <edii87shadow@gmail.com>
2025-02-06 10:09:03 +0200

    cerror v2 maintenance (#11)
    * cerror v2 maintenance

--- pkg/cerrl/cerrl.go
+++ pkg/cerrl/cerrl.go
@@ -4,6 +4,8 @@ package cerrl
 import (
 	"fmt"
 	"go/ast"
+	"regexp"
+
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
@@ -11,6 +13,11 @@ import (
 
 	"golang.org/x/tools/go/ast/inspector"
 )
+
+var _cerrorPkgPatternSuffixPaths = []string{
+	`/pkg/cerror(\"|$)`,
+	`/pkg/cerror/v([0-9]+)(\"|$)`,
+}
 
 func Analyzer() *analysis.Analyzer {
 	return &analysis.Analyzer{
@@ -36,9 +43,10 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		(*ast.CallExpr)(nil),
 	}
 
-	var cerrPkgName string
-
-	var isImportFinished bool
+	var (
+		cerrPkgName      string
+		isImportFinished bool
+	)
 
 	// filtered ast nodes will be passed here recursively
 	i.Preorder(nodeFilter, func(node ast.Node) {
@@ -51,7 +59,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				cerrPkgName = ""
 			}
 
-			if is.Path != nil && strings.HasSuffix(is.Path.Value, `/pkg/cerror"`) {
+			if is.Path != nil && hasSuffix(is.Path.Value, _cerrorPkgPatternSuffixPaths) {
 				// is.Name == nil means the import has no alias
 				if is.Name == nil {
 					cerrPkgName = "cerror"
@@ -108,6 +116,16 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	})
 
 	return nil, nil
+}
+
+func hasSuffix(path string, list []string) bool {
+	for _, pattern := range list {
+		if ok, _ := regexp.MatchString(pattern, path); ok {
+			return true
+		}
+	}
+
+	return false
 }
 
 // isCallExprWithNewCall checks that a given call expression is a cerror.New* call
