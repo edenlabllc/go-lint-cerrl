95d00f0448975af43abc2351ef9211f56159aa7c
edii <edii87shadow@gmail.com>
2025-11-06 11:21:25 +0200

    golang 1.25.4 and linter 1.64.8

--- docker/docker-compose-lint.sh
+++ docker/docker-compose-lint.sh
@@ -2,7 +2,7 @@
 
 cd /app
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8
 
 go build --buildmode plugin -o /tmp/cerrl.so plugin/cerrl.go
 
